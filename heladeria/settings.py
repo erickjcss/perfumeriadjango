ac6370414d60ca00a58ee5f3cc52e89f594a7699
erickjcss <151551835+erickjcss@users.noreply.github.com>
2025-10-03 18:45:17 -0400

    Update settings.py

--- heladeria/settings.py
+++ heladeria/settings.py
@@ -151,7 +151,7 @@ if not DEBUG:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_CREDENTIALS = True
-
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS=[
     "http://localhost:5173",
     "https://perfumeria-z0gh.onrender.com",
