2c5482abdf212a3446afc4aa60e25208fbbd1d3d
erickjcss <151551835+erickjcss@users.noreply.github.com>
2025-10-04 19:29:25 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,3 +6,7 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
 
+if [[ -z "$CREATE_SUPERUSER" ]]; then
+  python manage.py createsuperuser --no-input --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
+fi
+
