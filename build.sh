89b04190513054012c094a00ecbc6c04d25b3f0e
erickjcss <151551835+erickjcss@users.noreply.github.com>
2025-10-04 19:35:15 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py makemigrations
-python manage.py migrate
+python manage.py makemigrations Perfumes
+python manage.py migrate Perfumes
 
 if [[ -z "$CREATE_SUPERUSER" ]]; then
   python manage.py createsuperuser --no-input --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
